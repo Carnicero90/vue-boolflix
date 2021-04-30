792ca852dc209e5654d18765ebfa4314d30a9916
Calavera <calavera@localhost.localdomain>
2021-04-30 17:29:43 +0200

    rimosse funzioni da data

--- js/script.js
+++ js/script.js
@@ -4,18 +4,6 @@ const DOMAIN = 'https://api.themoviedb.org/3/';
 var app = new Vue({
     el: '#root',
     data: {
-        movieDBqueries: {
-            // TODO sposta in methods
-            search: function (type, args) {
-                return `${DOMAIN}search/${type}?api_key=${API_KEY}&query=${args}`
-            },
-            genres: function (type) {
-                return `${DOMAIN}genre/${type}/list?api_key=e987c9b3ef365c98fc145ab54f1b9e46&language=en-US`
-            },
-            byId: function (type, id, args) {
-                return `https://api.themoviedb.org/3/${type}/${id}?api_key=${API_KEY}${args}`
-            },
-        },
         selectedGen: '',
         queryString: '',
         selectedIndex: false,
@@ -53,46 +41,12 @@ var app = new Vue({
 
     },
     methods: {
-        searchContent(type, string) {
-            axios.get(this.movieDBqueries.search(this.kinds[type].queryStr, string))
-                .then((response) => {
-                    const answer = response.data.results;
-                    answer.forEach(element => {
-                        axios.get(this.movieDBqueries.byId(this.kinds[type].queryStr, element.id, '&append_to_response=credits'))
-                            .then(cast => {
-                                if (cast.data.credits.cast) { element.cast = cast.data.credits.cast.slice(0, 5) }
-                                if (cast.data.genres) { element.genres = cast.data.genres.map(item => item.name) }
-                            })
-                    });
-                    // this.kinds[type].searchResult = answer;
-                    //#TODO: migrare tutto in kinds
-                    this.result[type] = answer;
-
-                });
-        },
-        searchAll(string) {
-            // RESETS
-            this.result = {
-                'movies': [],
-                'tv': []
-            },
-                this.selectedGen = '';
-            for (el in this.kinds) {
-                this.kinds[el].collapsed = false;
-            }
-            // QUERIES
-            this.searchContent('movies', string);
-            this.searchContent('tv', string);
-            this.printed = this.result;
-
-        },
-
 
         getFlag(lang) {
             return this.flags[lang] || this.flags['default']
         },
         getCover(content) {
-            if(content.poster_path) {
+            if (content.poster_path) {
                 return `https://image.tmdb.org/t/p/w342/${content.poster_path}`
             }
             else {
@@ -123,12 +77,56 @@ var app = new Vue({
                 this.myList[kind].splice(this.myList[kind].indexOf(item), 1)
             }
         },
+        // API call functions
+        searchContent(type, string) {
+            axios.get(this.search(this.kinds[type].queryStr, string))
+                .then((response) => {
+                    const answer = response.data.results;
+                    answer.forEach(element => {
+                        axios.get(this.byId(this.kinds[type].queryStr, element.id, '&append_to_response=credits'))
+                            .then(cast => {
+                                if (cast.data.credits.cast) { element.cast = cast.data.credits.cast.slice(0, 5) }
+                                if (cast.data.genres) { element.genres = cast.data.genres.map(item => item.name) }
+                            })
+                    });
+                    // this.kinds[type].searchResult = answer;
+                    //#TODO: migrare tutto in kinds
+                    this.result[type] = answer;
+                });
+        },
+        searchAll(string) {
+            // RESETS
+            this.result = {
+                'movies': [],
+                'tv': []
+            },
+                this.selectedGen = '';
+            for (el in this.kinds) {
+                this.kinds[el].collapsed = false;
+            }
+            // QUERIES
+            this.searchContent('movies', string);
+            this.searchContent('tv', string);
+            this.printed = this.result;
+
+        },
         getGenres(kind) {
-            axios.get(this.movieDBqueries.genres(this.kinds[kind].queryStr))
+            axios.get(this.genres(this.kinds[kind].queryStr))
                 .then(response => {
                     this.kinds[kind].genres = response.data.genres.map(item => item.name);
                 });
-        }
+        },
+        // API query strings
+        // TODO: rinomina
+        search(type, args) {
+            return `${DOMAIN}search/${type}?api_key=${API_KEY}&query=${args}`
+        },
+        genres(type) {
+            return `${DOMAIN}genre/${type}/list?api_key=e987c9b3ef365c98fc145ab54f1b9e46&language=en-US`
+        },
+        byId(type, id, args) {
+            return `${DOMAIN}${type}/${id}?api_key=${API_KEY}${args}`
+        },
 
     },
     mounted() {
